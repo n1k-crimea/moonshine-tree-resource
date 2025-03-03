39f610cd5a1453e318e50d811fa3dea2e66e2998
Николай Евлошевский <evloshevsky@gmail.com>
2025-03-03 16:00:56 +0300

    иконки для unpublish и перетаскивания

--- resources/views/components/tree/item.blade.php
+++ resources/views/components/tree/item.blade.php
@@ -14,10 +14,12 @@
         <div class="flex justify-between items-center gap-4">
             <div class="@if($resource->sortable()) handle @endif flex justify-start items-center gap-4">
                 @if($resource->sortable())
-                    <x-moonshine::icon class="cursor-pointer" icon="heroicons.arrows-up-down" />
+                    <x-moonshine::icon class="cursor-pointer" icon="heroicons.arrows-pointing-out" />
                 @endif
                 @if($item->publish)
                     <x-moonshine::icon icon="heroicons.outline.check-circle" />
+                @else
+                    <x-moonshine::icon icon="heroicons.no-symbol" />
                 @endif
                 {{--                @if($item->parent_id == null || $item instanceof App\Models\CatalogRuSoftware || $item instanceof App\Models\CatalogOtherDevice)--}}
                 @if(false)
@@ -36,8 +38,8 @@
                 @endif
 
                 @if($resource->wrapable())
-                    <a aria-expanded={{count($item->childrens) > 0 ? "true" : "false"}} class="cursor-pointer" @click.stop="tree_show_{{ $item->getKey() }} = !tree_show_{{ $item->getKey() }}">
-                        <x-moonshine::icon icon="heroicons.arrow-down-on-square" />
+                    <a aria-expanded={{count($item->childrens) > 0 ? "true" : "false"}} aria-controls="tree-list-{{ $item->getKey() }}" class="cursor-pointer transition-transform aria-expanded:rotate-180">
+                        <x-moonshine::icon icon="heroicons.chevron-down" />
                     </a>
                 @endif
 
@@ -54,7 +56,7 @@
             <ul
                 @if($resource->sortable())
                     x-data="sortable('{{ $resource->route('sortable') }}', 'nested')"
-                class="dropzone"
+                class="dropzone aria-hidden:hidden"
                 x-show="tree_show_{{ $item->getKey() }}"
                 data-id="{{ $item->getKey() }}"
                 data-handle=".handle"
@@ -62,6 +64,7 @@
                 data-fallbackOnBody="true"
                 data-swapThreshold="0.65"
                 @endif
+                id="tree-list-{{ $item->getKey() }}"
                 aria-hidden={{count($item->childrens) > 0 ? "false" : "true"}}
             >
 
